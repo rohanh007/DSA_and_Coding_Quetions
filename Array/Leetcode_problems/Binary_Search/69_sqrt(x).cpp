7c25ee0987ec9ebf2eca0ac9a17f06883c4c6cb1
rohanh007 <77897898+rohanh007@users.noreply.github.com>
2024-06-21 10:19:18 +0530

    sqrt pbm

---
+++ Array/Leetcode_problems/Binary_Search/69_sqrt(x).cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0) return 0;
+        long long s = 1;
+        long long e = x;
+        int ans = 0;
+
+        while (s <= e) {
+            long long mid = s + (e - s) / 2;
+
+            if (mid * mid == x) {
+                return mid;
+            } else if (mid * mid < x) {
+                ans = mid;  
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
